b6899fad74b3d16e67fd4b0856cec8069621f477
joynab380-hue <sumayakarim380@gmail.com>
2025-10-31 13:45:09 +0600

    Update Grads.cpp

--- Grads.cpp
+++ Grads.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Friend
 {
     string name;
     int A, M, S, E;
+    int total;
 };
 bool compare(Friend f1, Friend f2)
 {
-    int total1 = f1.A + f1.M + f1.S + f1.E;
-    int total2 = f2.A + f2.M + f2.S + f2.E;
-
-    if (total1 != total2)
-        return total1 > total2;
+    if (f1.total != f2.total)
+        return f1.total > f2.total;
     return f1.name < f2.name;
 }
-
 int main()
 {
     int N;
@@ -27,6 +24,7 @@ int main()
     for (int i = 0; i < N; i++)
     {
         cin >> friends[i].name >> friends[i].A >> friends[i].M >> friends[i].S >> friends[i].E;
+        friends[i].total = friends[i].A + friends[i].M + friends[i].S + friends[i].E;
     }
 
     sort(friends.begin(), friends.end(), compare);
@@ -34,9 +32,8 @@ int main()
     for (int i = 0; i < N; i++)
     {
         cout << friends[i].name << " "
-             << friends[i].A << " "
-             << friends[i].M << " "
-             << friends[i].S << " "
+             << friends[i].total << " "<< friends[i].A << " "
+             << friends[i].M << " "<< friends[i].S << " "
              << friends[i].E << endl;
     }
     return 0;
